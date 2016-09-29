61d40232414c19cbebc9327696ddb3944984e8ab
TonyZT132 <tuo.zhang@mail.utoronto.ca>
2016-09-29 12:56:24 -0400

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :accounts
+  get "home/show" => "home#show"
+
+  root :to => "home#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
