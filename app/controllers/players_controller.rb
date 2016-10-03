cf19315e036600b6e17d9fcc5507f3b3f69fe4c9
Newton Lam <newton.lam@mail.utoronto.ca>
2016-10-02 23:15:54 -0400

    Create tournament players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,3 +1,17 @@
 class PlayersController < ApplicationController
 
+def create
+  	@player = Player.new
+  	@player.tournament_id = params[:tournament_id]
+  	@player.account_id = current_account.id
+  	#don't know what to do for groups and checkin yet
+
+
+  	if @player.save
+  		redirect_to tournaments_index_path, :notice => 'Joined Tournament'
+  	else
+  		render 'new', :notice => 'Invalid'
+  	end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
   get 'organizers/new'
+
+  post 'players/create'
 
   get 'tournaments/new'
   get 'tournaments/index'
