6f81f74a1bb63c687d6202b917cbd64020c51bcf
Yuxuan Zhang <edwinyuxuan.zhang@mail.utoronto.ca>
2016-10-04 23:24:53 -0400

    Fix one player can sign up for one tournment multiple time

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,18 +3,22 @@ class PlayersController < ApplicationController
 before_action :authenticate_account!, :except => [:show, :index]
 
 def create
-  	@player = Player.new
-  	@player.tournament_id = params[:tournament_id]
-  	@player.account_id = current_account.id
-  	#don't know what to do for groups and checkin yet
+    @player = Player.new
+    @player.tournament_id = params[:tournament_id]
+    @player.account_id = current_account.id
+    #don't know what to do for groups and checkin yet
+    @duplicate_player = Player.where(tournament_id: params[:tournament_id], account_id: @player.account_id)
+    if @duplicate_player.nil?
+      if @player.save
+        redirect_to tournaments_index_path, :notice => 'Joined Tournament'
+      else
+        render 'new', :notice => 'Invalid'
+      end
+    else
+        redirect_to tournaments_index_path, :notice => 'Already in Tournament'
+    end
+end
 
-
-  	if @player.save
-  		redirect_to tournaments_index_path, :notice => 'Joined Tournament'
-  	else
-  		render 'new', :notice => 'Invalid'
-  	end
-  end
 
 def show
     @player_info =  Player.where(tournament_id: params[:tournament_id])
