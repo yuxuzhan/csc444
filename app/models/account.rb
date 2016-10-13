c280a5282afc9205a3e0bb6b622b8eb596884dcf
Newton Lam <newton.lam@mail.utoronto.ca>
2016-10-12 23:17:58 -0400

    Change emails to lowercase before saving

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,13 @@
 class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  before_save :downcase_email
+
+  def downcase_email
+  	self.email.downcase!
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
