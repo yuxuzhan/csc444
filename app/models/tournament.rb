672fedc2e7741107139fcc302a143d1db25f181f
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-10-20 13:23:08 -0400

    try to fix layout

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,7 +2,6 @@ class Tournament < ApplicationRecord
 	validates :name, :presence => true
 	validates :venue, :presence => true
 	validates :details, :presence => true
-    validates :contact, :presence => true
-    validates :date, :presence => true
-
+  validates :contact, :presence => true
+  validates :date, :presence => true
 end
