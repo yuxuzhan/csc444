eb8f08ea888056579a73706704ea93b1620f5033
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-10-20 15:46:11 -0400

    better way to return gender info

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,16 @@ class ProfilesController < ApplicationController
 
 	def show
 		@account = Account.find_by id: current_account.id
+		@account.gender = returnGender(@account.gender)
 		@player = Player.where(account_id: current_account.id)
 		@organizer = Organizer.where(account_id: current_account.id)
+	end
+
+	def returnGender(genderInput)
+			if(genderInput == "m")
+					return "Male"
+			else
+					return "Female"
+			end
 	end
 end
