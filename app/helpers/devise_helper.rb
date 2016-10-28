507ddf78eca6f7aa2446e256562936fec3f69b21
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-10-28 14:28:07 -0400

    update devise_helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,18 +1,29 @@
 module DeviseHelper
-  def devise_error_messages!
-    return "" if resource.errors.empty?
+    def devise_error_messages!
+        html = ""
+        return html if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      count: resource.errors.count,
-                      resource: resource.class.model_name.human.downcase)
+        errors_number = 0
 
-    html = <<-HTML
-    <div id="error_explanation">
-      <h4>#{sentence}</h4>
-      <ul>#{messages}</ul>
-    </div>
-    HTML
-    html.html_safe
-  end
+        html << "<ul class=\"#{resource_name}_errors_list\">"
+
+        saved_key = ""
+        resource.errors.each do |key, value|
+            if key != saved_key
+                html << "<li class=\"#{key} error\"> This #{key} #{value} </li>"
+                errors_number += 1
+            end
+            saved_key = key
+        end
+
+        unsolved_errors = pluralize(errors_number, "unsolved errors")
+        if unsolved_errors > 1
+            html = "<h2 class=\"#{resource_name}_errors_title\"> You have #{unsolved_errors} </h2>" + html
+            html << "</ul>"
+            return html.html_safe
+        else
+            html = "<h2 class=\"#{resource_name}_errors_title\"> You have 1 unsolved error</h2>" + html
+            html << "</ul>"
+        end
+    end
 end
