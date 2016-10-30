378e79d906b016c4dc14444554828b118703bb76
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-10-29 22:05:21 -0400

    added check box on sponsor page

--- db/migrate/20160930022701_create_tournaments.rb
+++ db/migrate/20160930022701_create_tournaments.rb
@@ -1,13 +1,13 @@
 class CreateTournaments < ActiveRecord::Migration[5.0]
-  def change
-    create_table :tournaments do |t|
-    	t.string :name
-    	t.string :venue
-    	t.string :details
-    	t.string :contact
-    	t.boolean :private
-    	t.date :date
-      t.timestamps
+    def change
+        create_table :tournaments do |t|
+            t.string :name
+            t.string :venue
+            t.string :details
+            t.string :contact
+            t.boolean :private
+            t.date :date
+            t.timestamps
+        end
     end
-  end
 end
--- db/migrate/20161002030823_create_organizers.rb
+++ db/migrate/20161002030823_create_organizers.rb
@@ -1,10 +1,10 @@
 class CreateOrganizers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :organizers do |t|
-    	t.integer :tournament_id
-    	t.integer :account_id
-    	t.boolean :admin
-      t.timestamps
+    def change
+        create_table :organizers do |t|
+            t.integer :tournament_id
+            t.integer :account_id
+            t.boolean :admin
+            t.timestamps
+        end
     end
-  end
 end
