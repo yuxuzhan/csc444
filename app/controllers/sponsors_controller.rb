54fff0b81451821aab26c6fdd8576cefa81556c4
Yanrong Wang <yanrong.wang@mail.utoronto.ca>
2016-10-30 23:06:58 -0400

    updated the redirect link of sponsor

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -6,14 +6,18 @@ class SponsorsController < ApplicationController
     end
 
     def new
+      cookies[:tournament_id] = params[:tournament_id]
         @sponsor = Sponsor.new
     end
 
     def create
+
         @sponsor = Sponsor.new(sponsor_params)
         @sponsor.account_id = current_account.id
         if @sponsor.save
-          redirect_to tournaments_index_path, notice: 'Sponsor created'
+          tournament_id = cookies[:tournament_id]
+          cookies.delete :tournament_id
+          redirect_to sponsorships_new_path(:tournament_id => tournament_id), notice: 'Sponsor created'
         else
           render 'new', notice: 'Invalid'
         end
