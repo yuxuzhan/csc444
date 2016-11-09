e6f142cff2ce19ce282e0c5706a213eb35414c11
Yanrong Wang <yanrong.wang@mail.utoronto.ca>
2016-11-09 11:06:35 -0500

    modified sponsor image size and linkto

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -31,7 +31,7 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process resize_to_fit: [50, 50]
+     process resize_to_fit: [500, 500]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
