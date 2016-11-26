9c3d221b5084eeabe3c755cd28d83a971f74e226
Yanrong Wang <yanrong.wang@mail.utoronto.ca>
2016-11-26 17:06:55 -0500

    modified image size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-
+  process :resize_to_fit => [300, 300]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -14,14 +14,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_fill => [100, 100]
-  end
-
-  version :medium do
-    process :resize_to_fill => [300, 300]
-  end
-
-  version :small do
-    process :resize_to_fill => [140, 140]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
