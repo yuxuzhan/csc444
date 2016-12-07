03585b96d2c0061c087836ce6652bbab814cf980
Newton Lam <newton.lam@mail.utoronto.ca>
2016-12-07 01:43:41 -0500

    Fix arrange group/checkin for super admin

--- app/controllers/organizers_controller.rb
+++ app/controllers/organizers_controller.rb
@@ -16,15 +16,30 @@ class OrganizersController < ApplicationController
     end
 
     def get_played_players(tournament_id)
-        arr = []
-        tournament_obj = Tournament.find(tournament_id)
-        tournament_name = tournament_obj.name
-        player_info = Player.where(tournament_id: tournament_id)
-        arr << player_info
-        sub_admin_info = SubAdmin.where(tournament_id: tournament_id)
-        arr << sub_admin_info
-        @arrangement.tournaments_created[tournament_name] = arr
-        arr = []
+    	arr = []
+    	if(!current_account.admin)     
+	        tournament_obj = Tournament.find(tournament_id)
+	        tournament_name = tournament_obj.name
+	        player_info = Player.where(tournament_id: tournament_id)
+	        arr << player_info
+	        sub_admin_info = SubAdmin.where(tournament_id: tournament_id)
+	        arr << sub_admin_info
+	        @arrangement.tournaments_created[tournament_name] = arr
+	        arr = []
+	    else
+	    	organizer = Organizer.all
+	    	organizer.each do |organizer|
+	            tournament_id = organizer.tournament_id
+	            tournament_obj = Tournament.find_by id: tournament_id
+	            tournament_name = tournament_obj.name
+	            player_info = Player.where(tournament_id: tournament_id)
+	            arr << player_info
+	            sub_admin_info = SubAdmin.where(tournament_id: tournament_id)
+	            arr << sub_admin_info
+	            @arrangement.tournaments_created[tournament_name] = arr
+	            arr = []
+        	end
+	    end
     end
 end
 
